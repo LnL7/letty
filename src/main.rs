6072e79049cae9207c93c0985a4923b9a1d85455
Daiderd Jordan <daiderd@gmail.com>
2020-04-12 19:17:11 +0200

    attempt to resolve memory leaks

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use std::ptr;
 use std::thread;
 use std::time;
 
-use core_foundation::base::{kCFAllocatorDefault, CFGetRetainCount, CFRelease, CFType, TCFType, ToVoid};
+use core_foundation::base::{kCFAllocatorDefault, CFGetRetainCount, CFRetain, CFRelease, TCFType};
 use core_foundation::dictionary::{CFDictionary, CFDictionaryRef};
 use core_foundation::number::CFNumber;
 use core_foundation::array::{CFArrayGetCount, CFArrayGetValueAtIndex};
@@ -23,7 +23,7 @@ use core_foundation::string::{kCFStringEncodingUTF8, CFString, CFStringCreateWit
 use io_kit_sys::hid::base::*;
 use io_kit_sys::hid::device::*;
 use io_kit_sys::hid::element::*;
-use io_kit_sys::hid::keys::{kIOHIDOptionsTypeNone, kIOHIDDeviceUsagePageKey, kIOHIDDeviceUsageKey, kIOHIDProductKey, kIOHIDElementUsagePageKey};
+use io_kit_sys::hid::keys::{kIOHIDOptionsTypeNone, kIOHIDDeviceUsagePageKey, kIOHIDDeviceUsageKey, kIOHIDProductKey, kIOHIDElementUsagePageKey, kIOHIDElementUsageKey};
 use io_kit_sys::hid::manager::*;
 use io_kit_sys::hid::value::*;
 use io_kit_sys::hid::usage_tables::{kHIDPage_GenericDesktop, kHIDUsage_GD_Keyboard, kHIDUsage_LED_CapsLock, kHIDPage_LEDs};
@@ -37,20 +37,25 @@ impl_TCFType!(IOHIDManager, IOHIDManagerRef, IOHIDManagerGetTypeID);
 
 impl Drop for IOHIDManager {
     fn drop(&mut self) {
-        unsafe { CFRelease(self.as_CFTypeRef()) }
+        self.close();
+
         // eprintln!("  <IOHIDManager drop {}>", self.retain_count());
+        unsafe { CFRelease(self.as_CFTypeRef()) }
     }
 }
 
 impl IOHIDManager {
-    pub fn new() -> Option<Self> {
-        let m = unsafe { IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDManagerOptionNone) };
-
-        if m.is_null() {
-            None
-        } else {
-            Some(IOHIDManager(m))
+    pub fn new() -> Result<Self, io::Error> {
+        let value = unsafe { IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDManagerOptionNone) };
+        if value.is_null() {
+            return Err(io::Error::new(io::ErrorKind::Other, "failed to create manager"));
         }
+
+        let mut manager = IOHIDManager(value);
+        manager.open()
+            .map_err(|_e| io::Error::new(io::ErrorKind::Other, "failed to open manager"))?;
+
+        Ok(manager)
     }
 
     pub fn open(&mut self) -> Result<(), IOReturn> {
@@ -62,6 +67,7 @@ impl IOHIDManager {
     }
 
     pub fn close(&mut self) {
+        unsafe { IOHIDManagerSetDeviceMatching(self.0, ptr::null_mut()) }
         unsafe { IOHIDManagerClose(self.0, kIOHIDOptionsTypeNone); };
     }
 
@@ -76,8 +82,12 @@ impl IOHIDManager {
         } else {
             let device_count = unsafe { CFSetGetCount(devices_set) };
             let mut devices = Vec::with_capacity(device_count as usize);
-            let context = &mut devices as *mut _;
-            unsafe { CFSetApplyFunction(devices_set, vec_push_applier, context as *mut c_void) };
+            let context = &mut devices as *mut _ as *mut c_void;
+            unsafe { CFSetApplyFunction(devices_set, vec_push_applier, context) };
+
+            // eprintln!("  <CFSet release {}>", unsafe { CFGetRetainCount(devices_set as _) });
+            unsafe { CFRelease(devices_set as _) };
+
             devices
         }
     }
@@ -86,6 +96,10 @@ impl IOHIDManager {
 extern "C" fn vec_push_applier(value: *const c_void, context: *const c_void) {
     let vec = unsafe { &mut *(context as *mut Vec<IOHIDDevice>) };
     let device = IOHIDDevice(value as IOHIDDeviceRef);
+    // Moving ownersip of devices from the copied set to rust, since the array will be
+    // released before returning.
+    unsafe { CFRetain(device.as_CFTypeRef()) };
+    // eprintln!("  <IOHIDDevice retain {}>", unsafe { CFGetRetainCount(device.as_CFTypeRef()) });
     vec.push(device);
 }
 
@@ -97,14 +111,14 @@ impl_TCFType!(IOHIDDevice, IOHIDDeviceRef, IOHIDDeviceGetTypeID);
 
 impl Drop for IOHIDDevice {
     fn drop(&mut self) {
-        unsafe { CFRelease(self.as_CFTypeRef()) }
         // eprintln!("  <IOHIDDevice drop {}>", self.retain_count());
+        unsafe { CFRelease(self.as_CFTypeRef()) }
     }
 }
 
 impl IOHIDDevice {
-    fn get_matching_elements(&self, dict: &CFDictionary<CFString, CFNumber>) -> Result<Vec<IOHIDElement>, io::Error> {
-        let matching_dict = dict.to_untyped().to_void() as CFDictionaryRef;
+    pub fn get_matching_elements(&self, dict: &CFDictionary<CFString, CFNumber>) -> Result<Vec<IOHIDElement>, io::Error> {
+        let matching_dict = dict.as_CFTypeRef() as CFDictionaryRef;
         let elements = unsafe { IOHIDDeviceCopyMatchingElements(self.0, matching_dict, kIOHIDOptionsTypeNone) };
 
         if elements.is_null() {
@@ -121,35 +135,34 @@ impl IOHIDDevice {
             }
 
             let element = IOHIDElement(value as IOHIDElementRef);
+            // Moving ownersip of elements from the copied array to rust, since the array will be
+            // released before returning.
+            unsafe { CFRetain(element.as_CFTypeRef()) };
+            // eprintln!("  <IOHIDElement retain {}>", unsafe { CFGetRetainCount(element.as_CFTypeRef()) });
             vec.push(element);
         }
 
-        unsafe { CFRelease(elements as _) };
         // eprintln!("  <CFSet release {}>", unsafe { CFGetRetainCount(elements as _) });
+        unsafe { CFRelease(elements as _) };
 
         Ok(vec)
     }
 
-    fn get_name(&self) -> Option<String> {
+    pub fn get_name(&self) -> Option<String> {
         let product_key = unsafe { CFStringCreateWithCString(kCFAllocatorDefault, kIOHIDProductKey, kCFStringEncodingUTF8) };
         let property = unsafe { IOHIDDeviceGetProperty(self.0, product_key) };
+
+        unsafe { CFRelease(product_key as _) };
+
         if property.is_null() {
             None
         } else {
-            let value: CFType = unsafe { TCFType::wrap_under_get_rule(property) };
-            if value.instance_of::<CFString>() {
-                let value = unsafe { CFString::wrap_under_get_rule(value.as_CFTypeRef() as _) };
-                unsafe { CFRelease(value.as_CFTypeRef()) };
-                let name = value.to_string();
-                // eprintln!("  <CFString name {}>", value.retain_count());
-                Some(name)
-            } else {
-                None
-            }
+            let name = unsafe { CFString::wrap_under_get_rule(property as _) };
+            Some(name.to_string())
         }
     }
 
-    fn get_value(&self, element: &IOHIDElement) -> Result<i64, IOReturn> {
+    pub fn get_value(&self, element: &IOHIDElement) -> Result<i64, IOReturn> {
         let mut value: IOHIDValueRef = ptr::null_mut();
         let ret = unsafe { IOHIDDeviceGetValue(self.0, element.0, &mut value) };
 
@@ -161,12 +174,12 @@ impl IOHIDDevice {
         }
     }
 
-    fn set_value(&mut self, element: &mut IOHIDElement, new: i64) -> Result<i64, IOReturn> {
+    pub fn set_value(&mut self, element: &mut IOHIDElement, new: i64) -> Result<i64, IOReturn> {
         let value = unsafe { IOHIDValueCreateWithIntegerValue(kCFAllocatorDefault, element.0, 0, new) };
         let ret = unsafe { IOHIDDeviceSetValue(self.0, element.0, value) };
 
-        // unsafe { CFRelease(value as _) };
         // eprintln!("  <IOHIDValue release {}>", unsafe { CFGetRetainCount(value as _) });
+        unsafe { CFRelease(value as _) };
 
         match ret {
             kIOReturnSuccess => Ok(new),
@@ -174,7 +187,7 @@ impl IOHIDDevice {
         }
     }
 
-    fn toggle_value(&mut self, element: &mut IOHIDElement) -> Result<i64, IOReturn> {
+    pub fn toggle_value(&mut self, element: &mut IOHIDElement) -> Result<i64, IOReturn> {
         let current = self.get_value(&element)?;
         let min = unsafe { IOHIDElementGetLogicalMin(element.0) };
         let max = unsafe { IOHIDElementGetLogicalMax(element.0) };
@@ -190,13 +203,13 @@ impl_TCFType!(IOHIDElement, IOHIDElementRef, IOHIDElementGetTypeID);
 
 impl Drop for IOHIDElement {
     fn drop(&mut self) {
-        unsafe { CFRelease(self.as_CFTypeRef()) }
         // eprintln!("  <IOHIDElement drop {}>", self.retain_count());
+        unsafe { CFRelease(self.as_CFTypeRef()) }
     }
 }
 
 impl IOHIDElement {
-    fn is_led_capslock(&self) -> bool {
+    pub fn is_led_capslock(&self) -> bool {
         let page = unsafe { IOHIDElementGetUsagePage(self.0) };
         let usage = unsafe { IOHIDElementGetUsage(self.0) };
         kHIDPage_LEDs == page && usage == kHIDUsage_LED_CapsLock
@@ -215,46 +228,41 @@ fn keyboard_matching_dictionary() -> CFDictionary<CFString, CFNumber> {
     CFDictionary::from_CFType_pairs(&[(page_key, page_value), (usage_key, usage_value)])
 }
 
-fn led_matching_dictionary() -> CFDictionary<CFString, CFNumber> {
+fn capslock_matching_dictionary() -> CFDictionary<CFString, CFNumber> {
     let page_str = unsafe { CStr::from_ptr(kIOHIDElementUsagePageKey) };
     let page_key = CFString::from(page_str.to_str().unwrap());
     let page_value = CFNumber::from(kHIDPage_LEDs as i32);
 
-    CFDictionary::from_CFType_pairs(&[(page_key, page_value)])
+    let usage_str = unsafe { CStr::from_ptr(kIOHIDElementUsageKey) };
+    let usage_key = CFString::from(usage_str.to_str().unwrap());
+    let usage_value = CFNumber::from(kHIDUsage_LED_CapsLock as i32);
+
+    CFDictionary::from_CFType_pairs(&[(page_key, page_value), (usage_key, usage_value)])
 }
 
 
 fn main() -> Result<(), Box<dyn error::Error>> {
 
     let keyboards = keyboard_matching_dictionary();
-    let leds = led_matching_dictionary();
+    let leds = capslock_matching_dictionary();
 
-    let mut manager = IOHIDManager::new()
-        .ok_or(io::Error::new(io::ErrorKind::Other, "failed to create manager"))?;
-
-    manager.open()
-        .map_err(|_e| io::Error::new(io::ErrorKind::Other, "failed to open manager"))?;
-
-    loop {
+    for _ in 0..4 {
+        let mut manager = IOHIDManager::new()?;
         manager.set_device_matching(&keyboards);
 
         let mut devices = manager.get_devices();
         for device in &mut devices {
             // device.show();
 
-            // if let Some(name) = device.get_name() {
-            //     eprintln!("name {:?}", name);
-            // }
+            if let Some(name) = device.get_name() {
+                eprintln!("name {:?}", name);
+            }
 
             if let Ok(mut elements) = device.get_matching_elements(&leds) {
                 for element in &mut elements {
-                    // element.show();
-
-                    if element.is_led_capslock() {
-                        if let Ok(current) = device.get_value(&element) {
-                            if let Ok(updated) = device.toggle_value(element) {
-                                eprintln!("value {} -> {}", current, updated);
-                            }
+                    if let Ok(current) = device.get_value(&element) {
+                        if let Ok(updated) = device.toggle_value(element) {
+                            eprintln!("value {} -> {}", current, updated);
                         }
                     }
                 }
@@ -263,8 +271,6 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
         thread::sleep(time::Duration::from_millis(400));
     }
-
-    manager.close();
 
     Ok(())
 }
