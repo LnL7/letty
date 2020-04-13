372be41dda29b059fef0e82720f887e92581c5c5
Daiderd Jordan <daiderd@gmail.com>
2020-04-13 22:02:37 +0200

    update blink command

--- src/bin/letty-blink.rs
+++ src/bin/letty-blink.rs
@@ -10,30 +10,30 @@ fn main() -> Result<(), Box<dyn error::Error>> {
     let keyboards = keyboard_matching_dictionary();
     let leds = capslock_matching_dictionary();
 
-    for _ in 0..4 {
-        let mut manager = IOHIDManager::new()?;
-        manager.set_device_matching(&keyboards);
+    let mut manager = IOHIDManager::new()?;
+    manager.set_device_matching(&keyboards);
 
-        let mut devices = manager.get_devices();
-        for device in &mut devices {
-            // device.show();
+    let mut vec = Vec::with_capacity(1);
 
-            if let Some(name) = device.get_name() {
-                eprintln!("name {:?}", name);
-            }
-
-            if let Ok(mut elements) = device.get_matching_elements(&leds) {
-                for element in &mut elements {
-                    if let Ok(current) = device.get_value(&element) {
-                        if let Ok(updated) = device.toggle_value(element) {
-                            eprintln!("value {} -> {}", current, updated);
-                        }
-                    }
+    for mut device in manager.get_devices() {
+        if let Ok(elements) = device.get_matching_elements(&leds) {
+            for mut element in elements {
+                if let Ok(_updated) = device.set_value(&mut element, 0) {
+                    vec.push((device, element));
+                    break;
                 }
             }
         }
+    }
 
-        thread::sleep(time::Duration::from_millis(400));
+    for _ in 0..8 {
+        for (device, element) in &mut vec {
+            device.set_value(element, 1).unwrap();
+            thread::sleep(time::Duration::from_millis(800));
+
+            device.set_value(element, 0).unwrap();
+            thread::sleep(time::Duration::from_millis(200));
+        }
     }
 
     Ok(())
